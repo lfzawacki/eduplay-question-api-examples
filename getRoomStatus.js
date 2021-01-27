e2f651ad9b0a8f7b66d1b5161bbb56cc1cff5153
Lucas Zawacki <lfzawacki@gmail.com>
2021-01-27 16:07:41 -0300

    getRoomStatus to demonstrate /room_status call

---
+++ getRoomStatus.js
@@ -0,0 +1,29 @@
+const querystring = require('querystring');
+const sha1 = require('sha1');
+const axios = require('axios')
+
+const SERVER_URL = "https://lucas.elos.dev/transfer/room_status";
+const API_SECRET = process.argv[2];
+
+const generateChecksum = (params) => {
+  const qs = querystring.stringify({ ...params, secret: API_SECRET });
+  return sha1(qs);
+}
+
+const params = {
+  "internalMeetingID": process.argv[3],
+}
+
+params.checksum = generateChecksum(params);
+
+const statusPath = SERVER_URL + '/?' +  querystring.stringify(params);
+
+axios.get(statusPath)
+.then(res => {
+  console.log(res.data);
+})
+.catch(e => {
+  console.error(e);
+});
+
+
